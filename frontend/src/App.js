98246f98036c8c8586016a45fc5018489634d911
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-11-23 11:58:46 +0100

    Setup react eslint and lint files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export function App() {
-  return ( 
+export function App () {
+  return (
     <div>
       <h1>Hello user. cLoki frontend is under construction.</h1>
     </div>
